43bd510fe602e89cf5714bfc9675338d92659537
Kyle Uehlein <kyleuehlein@gmail.com>
2018-07-27 16:16:17 -0500

    feat: palindrome check in javascript

---
+++ strings/palindrome-check.js
@@ -0,0 +1,17 @@
+// time: O(n)
+// space: O(1)
+const isPalindrome = string => {
+  let left = 0
+  let right = string.length - 1
+
+  while (left <= right) {
+    if (string[left] !== string[right]) {
+      return false
+    }
+    left++
+    right--
+  }
+  return true
+}
+
+console.log(isPalindrome("abcdcba")) // true
