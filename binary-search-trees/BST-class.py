50fcffb368a28299efedc7c63818c1f64a77bff7
Kyle Uehlein <kyleuehlein@gmail.com>
2018-07-30 14:10:54 -0500

    chore: fix indenting

--- binary-search-trees/BST-class.py
+++ binary-search-trees/BST-class.py
@@ -32,7 +32,7 @@ class BST:
                 current = current.left
             elif value > current.value:
                 current = current.right
-      else:
+            else:
                 return True
         return False
 
@@ -41,36 +41,36 @@ class BST:
     def remove(self, value, parent = None):
         current = self
         while current is not None:
-      if value < current.value:
-        parent = current
-        current = current.left
-      elif value > current.value:
-        parent = current
-        current = current.right
-      else:
-        if current.left is not None and current.right is not None:
-          current.value = current.right.getMinValue()
-          current.right.remove(current.value, current)
-        elif parent is None:
-          if current.left is not None:
-            current.value = current.left.value
-            current.right = current.left.right
-            current.left = current.left.left
-          elif current.right is not None:
-            current.value = current.right.value
-            current.left = current.right.left
-            current.right = current.right.right
-          else:
-            current.value = None
-        elif parent.left == current:
-          parent.left = current.left if current.left is not None else current.right
-        elif parent.right == current:
-          parent.right = current.left if current.left is not None else current.right
-        break
+            if value < current.value:
+                parent = current
+                current = current.left
+            elif value > current.value:
+                parent = current
+                current = current.right
+            else:
+              if current.left is not None and current.right is not None:
+                  current.value = current.right.getMinValue()
+                  current.right.remove(current.value, current)
+              elif parent is None:
+                  if current.left is not None:
+                      current.value = current.left.value
+                      current.right = current.left.right
+                      current.left = current.left.left
+                  elif current.right is not None:
+                      current.value = current.right.value
+                      current.left = current.right.left
+                      current.right = current.right.right
+                  else:
+                      current.value = None
+              elif parent.left == current:
+                  parent.left = current.left if current.left is not None else current.right
+              elif parent.right == current:
+                  parent.right = current.left if current.left is not None else current.right
+              break
         return self
 
   def getMinValue(self):
-    current = self
-    while current.left is not None:
-      current = current.left
-    return current.value
+      current = self
+      while current.left is not None:
+          current = current.left
+      return current.value
